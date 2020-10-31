681ca2cf6c22a2ee71f6e3f9360f670d2c449e0e
Jesus Duran Huancas <jesusgonzaloduran@gmail.com>
2020-10-31 02:36:49 -0500

    enable discovery client

--- src/main/java/com/evertix/sessionservice/SessionServiceApplication.java
+++ src/main/java/com/evertix/sessionservice/SessionServiceApplication.java
@@ -1,6 +1,7 @@
 package com.evertix.sessionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class SessionServiceApplication {
 
     public static void main(String[] args) {
